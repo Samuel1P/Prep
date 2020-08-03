594e959b3ff62f46b9322803b2af7702216f882e
Samuel1P <ipsamuelip@gmail.com>
2020-08-03 13:46:19 +0530

    adding one the frequently asked problem. One loop variant is easier.

---
+++ small_problems/find_pairs_equals_to_sum_one_loop.py
@@ -0,0 +1,14 @@
+arr_ = [1,9,2,4,5,6,7,8,11]
+tot_ = 12
+
+def find_pair(arr, tot):
+    ref_list = []
+    pair_list = []
+    for num in arr:
+        if tot - num in ref_list:
+            pair_list.append((tot-num, num))
+        else:
+            ref_list.append(num)
+    return pair_list
+
+print (find_pair(arr_, tot_))
---
+++ small_problems/find_pairs_equals_to_sum_two_loop.py
@@ -0,0 +1,14 @@
+arr_ = [1,9,2,4,5,6,7,8,11]
+tot_ = 12
+
+def find_pair(arr, tot):
+    size = len(arr)
+    pair_list = []
+    for i in range(size):
+        for j in range(i+1,size):
+            if arr[i]+arr[j] == tot:
+                pair_list.append((arr[i],arr[j]))
+    return pair_list
+
+
+print (find_pair(arr_, tot_))
