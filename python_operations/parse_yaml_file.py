520ff468c0836d637a61df5b9eedeed5ec973c60
Samuel1P <ipsamuelip@gmail.com>
2020-08-08 20:54:25 +0530

    yaml parser - read and write

--- python_operations/parse_yaml_file.py
+++ python_operations/parse_yaml_file.py
@@ -1,17 +1,20 @@
 import yaml
+to_write = {'template': {'metadata': {'name': 'my-app-dep', 'labels': {'env': 'stage', 'tier': 'backend'}}, 'spec': {'containers': [{'name': 'tiny-http', 'image': 'hjacobs/tiny-docker-http-test'}]}}}
+
 
 def read_yaml(fname):
     obj  = open(fname, "r")
-    parsed = yaml.safe_load_all(obj)
-    return parsed
+    parsed_gen = yaml.safe_load_all(obj) # Creates a generator of all Yaml Items in a file
+    return parsed_gen
 
-def write_yaml(fname, fobj):
-    yaml.dump()
+def write_yaml(i, fname):
+    with open(fname, "w") as fobj:
+        yaml.dump(i, fobj)
 
-def parse_file(fobj):
-    print(next(fobj))
-
+def print_file(fobj):
+    for yam in fobj:
+        print (yam)
 
 obj = read_yaml("sample.yaml")
-parse_file(obj)
-
+print_file(obj)
+write_yaml(to_write,"write.yaml")
