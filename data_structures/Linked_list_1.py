5e5355605469094281557443996f9109a6311cfc
Samuel1P <ipsamuelip@gmail.com>
2020-08-16 21:44:16 +0530

    Update Linked_list_1.py
    - added third node.
    - changed str to repr

--- data_structures/Linked_list_1.py
+++ data_structures/Linked_list_1.py
@@ -4,14 +4,17 @@ class node:
         self.element = data
         self.reference = ref
 
-    def __str__(self):
+    def __repr__(self):
         return f"node('{self.element}', {self.reference})"
 
     def node_contents(self):
-        print(f" data :{self.element} -- ref: {self.reference.__str__()}")
+        print(f" data :{self.element} -- ref: {self.reference.__repr__()}")
 
 node1 = node('data1', None)
 node2 = node('data2', None)
+node3 = node('data3', None)
 node1.reference = node2
+node2.reference = node3
 node1.node_contents()
 node2.node_contents()
+node3.node_contents()
